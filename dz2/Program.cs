8b14c3c3da3bb1e9a101b6cd54555f32f6cca22b
Anton <Scccq@yandex.ru>
2023-02-28 10:32:37 +0300

    Добавили ДЗ2

---
+++ dz2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+
+if (number[0] == '-')
+{
+    if (number.Length >= 4)
+    {
+        Console.WriteLine(number[3]);
+    }
+    else
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+}
+else
+{
+    if (number.Length >= 3)
+    {
+        Console.WriteLine(number[2]);
+    }
+    else
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+}
